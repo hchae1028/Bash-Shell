dbef098800d718d0faad7c2caa7f76e4c3b43ee2
hchae1028 <caihoyoung27@gmail.com>
2026-04-30 02:34:08 -0600

    Annotated

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,6 +1,14 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 
+/**
+ * @brief Splits a command line string into shell arguments.
+ *        Handles whitespace, single quotes, double quotes, and backslash escaping.
+ *        Returns the number of parsed arguments.
+ * @param arg (char *) Command line string to tokenize.
+ * @param argv (char *[]) Output array that stores pointers to each argument.
+ * @param max_args (int) Maximum number of arguments argv can hold.
+ */
 int tokenize_arg(char *arg, char *argv[], int max_args);
 
 #endif
