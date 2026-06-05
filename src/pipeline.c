d51abaf7d4ccd8165029e1ee54c059de55b10567
hchae1028 <caihoyoung27@gmail.com>
2026-06-05 17:44:03 -0600

    multiple pipelines

--- src/pipeline.c
+++ src/pipeline.c
@@ -8,43 +8,68 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
-static int process_pipelined_command(pid_t *pid, int target_fd, int pipefd[2], char *argv[], Redirection *redir);
-static int split_pipeline(char *argv[], char ***left_argv, char ***right_argv);
+static int process_pipelined_command(pid_t *pid, char *segment[], int prev, int pipefd[2], int has_next, int is_first, int is_last, Redirection *redir);
+static int split_pipeline_seg(char *argv[], char **segments[]);
 static int get_argc(char *argv[]);
 
 void execute_pipeline(char *argv[], Redirection *redir) {
-    char **left_argv = NULL;
-    char **right_argv = NULL;
-    int pipefd[2];
-    pid_t left_pid, right_pid;
+    char **segments[MAX_PIPELINE];
+    pid_t pids[MAX_PIPELINE];
+    int segment_count;
+    int pid_count = 0;
     int status;
+    int prev = -1;
 
     if (redir->in_file && access(redir->in_file, R_OK) == -1) {
         perror(redir->in_file);
         return;
     }
 
-    if (!split_pipeline(argv, &left_argv, &right_argv))
-        return;
-    if (pipe(pipefd) == -1)
+    segment_count = split_pipeline_seg(argv, segments);
+    if (segment_count == -1)
         return;
 
-    if (process_pipelined_command(&left_pid, STDOUT_FILENO, pipefd, left_argv, redir)) {
-        close(pipefd[0]);
-        close(pipefd[1]);
-        return;
-    }
-    if (process_pipelined_command(&right_pid, STDIN_FILENO, pipefd, right_argv, redir)) {
-        close(pipefd[0]);
-        close(pipefd[1]);
-        waitpid(left_pid, &status, 0);
-        return;
+    for (int i = 0; i < segment_count; i++) {
+        int pipefd[2] = {-1, -1};
+        int has_next = (i < segment_count - 1);
+
+        if (has_next && pipe(pipefd) == -1) {
+            perror("pipe");
+            if (prev != -1)
+                close(prev);
+            break;
+        }
+
+        if (process_pipelined_command(&pids[pid_count], segments[i], prev, pipefd, has_next,
+                                      i == 0, i == segment_count - 1, redir) == -1) {
+            perror("fork");
+            if (prev != -1)
+                close(prev);
+            if (pipefd[0] != -1)
+                close(pipefd[0]);
+            if (pipefd[1] != -1)
+                close(pipefd[1]);
+            break;
+        }
+        pid_count++;
+
+        if (prev != -1)
+            close(prev);
+
+        if (has_next) {
+            close(pipefd[1]);
+            prev = pipefd[0];
+        }
+        else {
+            prev = -1;
+        }
     }
 
-    close(pipefd[0]);
-    close(pipefd[1]);
-    waitpid(left_pid, &status, 0);
-    waitpid(right_pid, &status, 0);
+    if (prev != -1)
+        close(prev);
+
+    for (int i = 0; i < pid_count; i++)
+        waitpid(pids[i], &status, 0);
 }
 
 /**
@@ -60,118 +85,123 @@ int find_pipeline_index(char *argv[]) {
     return -1;
 }
 
-static int process_pipelined_command(pid_t *pid, int target_fd, int pipefd[2], char *argv[], Redirection *redir) {
+static int process_pipelined_command(pid_t *pid, char *segment[], int prev, int pipefd[2], int has_next, int is_first, int is_last, Redirection *redir) {
     *pid = fork();
 
     if (*pid == -1)
         return -1;
 
     if (*pid == 0) {
-        if (target_fd == STDOUT_FILENO) {
-            if (redir->in_file) {
-                int fd = open(redir->in_file, O_RDONLY);
-                if (fd == -1) {
-                    perror(redir->in_file);
-                    _exit(1);
-                }
-
-                if (dup2(fd, STDIN_FILENO) == -1) {
-                    perror("dup2");
-                    _exit(1);
-                }
-                close(fd);
+        if (is_first && redir->in_file) {
+            int fd = open(redir->in_file, O_RDONLY);
+            if (fd == -1) {
+                perror(redir->in_file);
+                _exit(1);
             }
+            if (dup2(fd, STDIN_FILENO) == -1) {
+                close(fd);
+                perror("dup2");
+                _exit(1);
+            }
+            close(fd);
+        }
+        else if (prev != -1) {
+            if (dup2(prev, STDIN_FILENO) == -1) {
+                close(prev);
+                perror("dup2");
+                _exit(1);
+            }
+        }
 
+        if (has_next) {
             if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
                 perror("dup2");
                 _exit(1);
             }
         }
-        else if (target_fd == STDIN_FILENO) {
-            if (dup2(pipefd[0], STDIN_FILENO) == -1) {
+        else if (is_last && redir->out_file) {
+            int flags = O_CREAT | O_WRONLY;
+            if (redir->out_append)
+                flags |= O_APPEND;
+            else
+                flags |= O_TRUNC;
+
+            int fd = open(redir->out_file, flags, 0644);
+            if (fd == -1) {
+                perror(redir->out_file);
+                _exit(1);
+            }
+            if (dup2(fd, STDOUT_FILENO) == -1) {
+                close(fd);
                 perror("dup2");
                 _exit(1);
             }
-
-            if (redir->out_file) {
-                int flags = O_CREAT | O_WRONLY;
-                if (redir->out_append)
-                    flags |= O_APPEND;
-                else
-                    flags |= O_TRUNC;
-
-                int fd = open(redir->out_file, flags, 0644);
-                if (fd == -1) {
-                    perror(redir->out_file);
-                    _exit(1);
-                }
-                if (dup2(fd, STDOUT_FILENO) == -1) {
-                    perror("dup2");
-                    _exit(1);
-                }
-                close(fd);
-            }
-
-            if (redir->err_file) {
-                int flags = O_CREAT | O_WRONLY;
-                if (redir->err_append)
-                    flags |= O_APPEND;
-                else
-                    flags |= O_TRUNC;
-
-                int fd = open(redir->err_file, flags, 0644);
-                if (fd == -1) {
-                    perror(redir->err_file);
-                    _exit(1);
-                }
-                if (dup2(fd, STDERR_FILENO) == -1) {
-                    perror("dup2");
-                    _exit(1);
-                }
-                close(fd);
-            }
+            close(fd);
         }
 
-        close(pipefd[0]);
-        close(pipefd[1]);
+        if (is_last && redir->err_file) {
+            int flags = O_CREAT | O_WRONLY;
+            if (redir->err_append)
+                flags |= O_APPEND;
+            else
+                flags |= O_TRUNC;
 
-        if (is_builtin(argv[0])) {
+            int fd = open(redir->err_file, flags, 0644);
+            if (fd == -1) {
+                perror(redir->err_file);
+                _exit(1);
+            }
+            if (dup2(fd, STDERR_FILENO) == -1) {
+                close(fd);
+                perror("dup2");
+                _exit(1);
+            }
+            close(fd);
+        }
+
+        if (pipefd[0] != -1)
+            close(pipefd[0]);
+        if (pipefd[1] != -1)
+            close(pipefd[1]);
+        if (prev != -1)
+            close(prev);
+
+        if (is_builtin(segment[0])) {
             char pathbuf[COMMAND_SIZE];
-            int argc = get_argc(argv);
+            int argc = get_argc(segment);
 
-            if (run_builtin(pathbuf, sizeof(pathbuf), argc, argv)) {
+            if (run_builtin(pathbuf, sizeof(pathbuf), argc, segment)) {
                 fflush(stdout);
                 fflush(stderr);
                 _exit(0);
             }
         }
-        execvp(argv[0], argv);
-        perror(argv[0]);
+        execvp(segment[0], segment);
+        perror(segment[0]);
         _exit(127); // error code 127: command not found
     }
     return 0;
 }
 
-static int split_pipeline(char *argv[], char ***left_argv, char ***right_argv) {
-    int pipe_index = find_pipeline_index(argv);
+static int split_pipeline_seg(char *argv[], char **segments[]) {
+    int count = 1;
+    segments[0] = argv;
 
-    if (pipe_index == -1)
-        return 0;
-    /* Missing left command; invalid */
-    if (pipe_index == 0) {
-        printf("parse error\n");
-        return 0;
+    for (int i = 0; argv[i] != NULL; i++) {
+        if (strcmp(argv[i], "|") == 0) {
+            if (i == 0 || argv[i + 1] == NULL || strcmp(argv[i + 1], "|") == 0) {
+                printf("parse error\n");
+                return -1;
+            }
+            if (count >= MAX_PIPELINE) {
+                printf("parse error: too many pipeline commands\n");
+                return -1;
+            }
+            argv[i] = NULL;
+            segments[count++] = &argv[i + 1];
+        }
     }
-    /* Missing right command; invalid */
-    if (argv[pipe_index + 1] == NULL) {
-        printf("parse error\n");
-        return 0;
-    }
-
-    argv[pipe_index] = NULL;
-    *left_argv = argv;
-    *right_argv = &argv[pipe_index + 1];
-    return 1;
+    return count;
 }
 
 static int get_argc(char *argv[]) {
